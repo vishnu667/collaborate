30d623547e219821d9cfb5cf803b10dc52ac6bf0
vishnu prasad s <vishnu667@gmail.com>
2015-03-09 22:50:08 +0530

    Update Main.scala

--- src/main/scala/in/cybergen/collaborate/Main.scala
+++ src/main/scala/in/cybergen/collaborate/Main.scala
@@ -3,13 +3,5 @@ package in.cybergen.collaborate
 object Main {
 	def main(args: Array[String]) {
     	println("Starting Application")
-        main = new Main()
-        main.hi()
     }
  }
-
-class Main {
-def hi():Unit = {
-println(" hey how are you ?")
-}
-}
