d8836de772440294dc42fef4cdda4137467a5906
vishnu prasad <vishnu667@gmail.com>
2015-03-09 22:44:54 +0530

    initial commit

---
+++ src/main/scala/in/cybergen/collaborate/Main.scala
@@ -0,0 +1,15 @@
+package in.cybergen.collaborate
+
+object Main {
+	def main(args: Array[String]) {
+    	println("Starting Application")
+        main = new Main()
+        main.hi()
+    }
+ }
+
+class Main {
+def hi():Unit = {
+println(" hey how are you ?")
+}
+}
