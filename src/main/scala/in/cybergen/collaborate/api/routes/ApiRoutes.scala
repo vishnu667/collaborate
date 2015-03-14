3d88fd5e6bdb9b9c159c31618fbe832b4fba3022
vishnu prasad <vishnu667@gmail.com>
2015-03-14 23:53:54 +0530

    Maven Build Issue Fixed removed the DB connector from the import statements

--- src/main/scala/in/cybergen/collaborate/api/routes/ApiRoutes.scala
+++ src/main/scala/in/cybergen/collaborate/api/routes/ApiRoutes.scala
@@ -1,14 +1,13 @@
 package in.cybergen.collaborate.api.routes
 
 
-import in.cybergen.collaborate.db.DB
 import in.cybergen.collaborate.models.{User, AuthenticationModel}
 import spray.routing._
 
 /**
  * Created by vishnu on 10/3/15.
  */
-trait ApiRoutes extends HttpService with AuthenticationModel with DB {
+trait ApiRoutes extends HttpService with AuthenticationModel {
 
   def apiRoutes =
       path("user") {
